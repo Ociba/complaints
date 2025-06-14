ae7d9ebb862d454051f7b6e272c5f82213b2f709
Ociba <ocibajames@gmail.com>
2025-06-14 05:09:31 +0300

    footer

--- resources/views/layouts/front/footer.blade.php
+++ resources/views/layouts/front/footer.blade.php
@@ -50,13 +50,13 @@
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Save Me</strong> <span>All Rights Reserved</span></p>
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Tuliwamu</strong> <span>All Rights Reserved</span></p>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="#">Kishan IT</a>
+        Designed by <a href="https://kishanit.com/">Kishan IT Solutions</a>
       </div>
     </div>
 
